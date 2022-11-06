7ed9fc6372cbcfd19dd5d2a1196afe16e5462a2c
Enamhossain <bhaianam9@gmail.com>
2022-11-06 23:57:02 +0600

    about add

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <p className='  text-2xl font-bold text-orange-600'>Service</p>
+                  <h2 className='text-5xl font-semibold'> Our Service Area </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
